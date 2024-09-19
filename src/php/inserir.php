4e6df86bfbabbed4f36e40c02d4eb1fee08d9d20
Lopes5681 <npccomsentimentos@gmail.com>
2024-09-19 13:47:35 -0300

    md5

--- src/php/inserir.php
+++ src/php/inserir.php
@@ -1,7 +1,7 @@
 <?php
 $nome = $_POST["nome"];
 $email= $_POST["email"];
-$senha= $_POST["senha"];
+$senha= md5($_POST["senha"]);
 
 include("conexao.php");
 $con-> query("insert into bd_receita.usuarios (nome, email, senha) values ('$nome', '$email', '$senha')");
